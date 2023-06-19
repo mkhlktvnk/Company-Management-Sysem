31540ad3d8ec13274e39cd1a1545766d28ecf943
mhlktvnk <mikhail.kutovenko@gmail.com>
2023-06-20 00:57:17 +0300

    Add message keys

---
+++ src/main/java/com/digitalchief/companymanagement/message/key/CompanyMessageKey.java
@@ -0,0 +1,9 @@
+package com.digitalchief.companymanagement.message.key;
+
+public class CompanyMessageKey {
+
+    public static final String NOT_FOUND_BY_ID = "company.not-found.by-id";
+
+    public static final String ALREADY_EXISTS_BY_EMAIL = "company.already-exists.by-name";
+
+}
---
+++ src/main/java/com/digitalchief/companymanagement/message/key/DepartmentMessageKey.java
@@ -0,0 +1,7 @@
+package com.digitalchief.companymanagement.message.key;
+
+public class DepartmentMessageKey {
+
+    public static final String DEPARTMENT_NOT_FOUND_BY_ID = "department.not-found.by-id";
+
+}
