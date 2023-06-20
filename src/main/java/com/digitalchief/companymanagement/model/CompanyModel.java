1f6b66edf803fa007449bdddcfa70bcff7fe294b
mhlktvnk <mikhail.kutovenko@gmail.com>
2023-06-20 08:47:03 +0300

    Implement models

--- src/main/java/com/digitalchief/companymanagement/model/CompanyModel.java
+++ src/main/java/com/digitalchief/companymanagement/model/CompanyModel.java
@@ -1,4 +1,36 @@
 package com.digitalchief.companymanagement.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CompanyModel {
+
+    @JsonProperty(value = "id", access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @NotEmpty
+    @JsonProperty(value = "name")
+    private String name;
+
+    @NotEmpty
+    @JsonProperty(value = "description")
+    private String description;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonProperty(value = "dateOfCreation")
+    private LocalDate dateOfCreation;
+
+    @JsonProperty(value = "numberOfEmployees", access = JsonProperty.Access.READ_ONLY)
+    private Long numberOfEmployees;
 }
--- src/main/java/com/digitalchief/companymanagement/model/DepartmentModel.java
+++ src/main/java/com/digitalchief/companymanagement/model/DepartmentModel.java
@@ -1,4 +1,35 @@
 package com.digitalchief.companymanagement.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DepartmentModel {
+
+    @JsonProperty(value = "id", access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @NotEmpty
+    @JsonProperty(value = "name")
+    private String name;
+
+    @NotEmpty
+    @JsonProperty(value = "description")
+    private String description;
+
+    @Positive
+    @Digits(integer = 19, fraction = 2)
+    @JsonProperty(value = "annualBudget")
+    private BigDecimal annualBudget;
 }
