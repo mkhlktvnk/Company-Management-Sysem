914435843211d17c8a7247910fb4c6a10ce9ac42
mhlktvnk <mikhail.kutovenko@gmail.com>
2023-06-20 05:34:11 +0300

    Update repos

--- src/main/java/com/digitalchief/companymanagement/repository/DepartmentRepository.java
+++ src/main/java/com/digitalchief/companymanagement/repository/DepartmentRepository.java
@@ -1,9 +1,18 @@
 package com.digitalchief.companymanagement.repository;
 
 import com.digitalchief.companymanagement.entity.Department;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    List<Department> findAllByCompanyId(Long companyId, Pageable pageable);
+
+    List<Department> findAllByCompanyIdAndId(Long companyId, Long departmentId);
+
+    Optional<Department> findByCompanyIdAndId(Long companyId, Long id);
 }
